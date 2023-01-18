6928a4df6d476e0e7ac9aa73c6cd376ca8ebb08f
Owen Riske <owenr0129@5565.ca>
2023-01-18 08:08:50 -0700

    Commit 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,22 +9,28 @@ public class Main {
         Scanner INPUT = new Scanner(System.in);
         String userInput;
         while (true) {
-            System.out.println("1. See Movie List\n2. Find Specific Movie\n3. Admin Login");
+            System.out.print("\n1. See Movie List\n2. Find Specific Movie\n3. Admin Login\n>");
             userInput = INPUT.nextLine();
             if (userInput.equalsIgnoreCase("See Movie List") || userInput.equalsIgnoreCase("Movie List") || userInput.equalsIgnoreCase("1")) {
 
             } else if (userInput.equalsIgnoreCase("Find Specific Movie") || userInput.equalsIgnoreCase("Find Movie") || userInput.equalsIgnoreCase("2")) {
 
             } else if (userInput.equalsIgnoreCase("Admin Login") || userInput.equalsIgnoreCase("Admin") || userInput.equalsIgnoreCase("3")) {
-                while (userInput != adminPass || userInput != "quit") {
+                while (!userInput.equals(adminPass) && !userInput.equalsIgnoreCase("quit")) {
                     System.out.print("\nPassword\n>");
                     userInput = INPUT.nextLine();
                 }
-                if (userInput == adminPass) {
+                if (userInput.equalsIgnoreCase(adminPass)) {
                     while (true) {
-                        System.out.println("1. Add Movie");
+                        System.out.print("\n1. Add Movie\n 2. Delete Movie\n>");
                         userInput = INPUT.nextLine();
-                        if (userInput.equalsIgnoreCase("Add Movie")||userInput.equalsIgnoreCase("Add")|| userInput.equalsIgnoreCase("1"))
+                        if (userInput.equalsIgnoreCase("Add Movie")||userInput.equalsIgnoreCase("Add")|| userInput.equalsIgnoreCase("1")){
+                            System.out.print("Movie Name\n>");
+                            movie.makeMovie(INPUT);
+                        }
+                        else if (userInput.equalsIgnoreCase("Delete Movie")||userInput.equalsIgnoreCase("Delete")||userInput.equalsIgnoreCase("2")){
+
+                        }
                     }
                 }
 
--- src/com/company/movie.java
+++ src/com/company/movie.java
@@ -1,56 +1,44 @@
 package com.company;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.util.Scanner;
 
 public class movie {
-    String name;
-    int rating;
-    double length;
-    String description;
-
-    //constructor
-    movie(String name, String description, double length, int rating){
-        this.name=name;
-        this.description=description;
-        this.length=length;
-        this.rating=rating;
+    public static void makeMovie(Scanner INPUT){
+        System.out.print("\nMovie Name\n>");
+        String name=INPUT.nextLine();
+        System.out.print("\nMovie Description\n>");
+        String description=INPUT.nextLine();
+        System.out.print("\nMovie Length\n>");
+        double length=INPUT.nextDouble();
+        System.out.print("\nRating\n>");
+        int rating=INPUT.nextInt();
         movieFile(name,description,length,rating);
-    }
-    movie(String name, String description, int rating){
-        this.name=name;
-        this.description=description;
-        this.length=0.0;
-        this.rating=rating;
-        movieFile(name,description,length,rating);
+
     }
 
-    String getName(){
-        return name;
-    }
-
-    void movieFile(String name, String description, double length, int rating){
+    private static void movieFile(String name, String description, double length, int rating){
         File f = new File("F:\\"+name+".txt");
 
         try {
             //create and write movie file
             if (f.createNewFile()){
                 System.out.println(name + " has been created");
-            FileWriter fWrite = new FileWriter(name+".txt");
-            fWrite.write("Name: "+name+"\nDescription: "+description+"\nLength: "+length+"\nRating: "+rating); }
+                FileWriter fWrite = new FileWriter(name+".txt");
+                fWrite.write("Name: "+name+"\nDescription: "+description+"\nLength: "+length+"\nRating: "+rating); }
             //movie is already saved
             else{
                 System.out.println(name + " already exists");
+            }
         }
-                   }
         catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        //file stuff from:
+        //https://www.w3schools.com/java/java_files_create.asp
+        //https://www.geeksforgeeks.org/file-class-in-java/
     }
-
-    }
-
-
-
+}
